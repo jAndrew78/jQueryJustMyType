ea41340de49697deb3ffcfeb34e2b4696b712339
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2020-09-23 19:06:46 -0500

    made sentences array, set global variable 'i' to track through sentenced array, appended first sentence to #sentence div

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // GLOBAL HIDE UPPER KEYBOARD ON PAGE LOAD
 $('#keyboard-upper-container').hide();
 
+let i = 0;
+$('#sentence').append(sentences[i]);
+
+
 // TOGGLE UPPER AND LOWER KEYBOARDS ON SHIFT KEYDOWN/UP
 $(document).keydown(function(e) {
     if (e.keyCode == 16) {
@@ -14,15 +18,34 @@ $(document).keydown(function(e) {
     }
 });
 
-// HIGHLIGHT KEYS IN BROWSER WHEN PRESSED ON KEYBOARD
+// HIGHLIGHT KEYS IN BROWSER WHEN PRESSED ON KEYBOARD, WAIT, REMOVE
 $(document).keypress(function(event) {
     let keyPress = event.keyCode || event.which;
     console.log(keyPress);
     $(`#${keyPress}`).css({backgroundColor: 'mediumslateblue'});
     setTimeout(() => {
-        $(`#${keyPress}`).css({backgroundColor: 'white'});
+        $(`#${keyPress}`).css({backgroundColor: 'rgb(0,0,0,0'});
     }, 100);
 });
+
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 
+                    'Too ato too nOt enot one totA not anot tOO aNot', 
+                    'oat itain oat tain nate eate tea anne inant nean', 
+                    'itant eate anot eat nato inate eat anot tain eat', 
+                    'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+ 
+
+
+
+// setTimeout(() => { }, 1000);
+
+
+// for (let i = 0; i < sentences.length; i++) {
+//     const x = sentences[i];
+//     $('#sentence').append(x);
+//     setTimeout(() => { }, 1000);
+// }
 
 // // REMOVE HIGHLIGHTING ON KEYUP
 // $(document).keyup(function(event) {
