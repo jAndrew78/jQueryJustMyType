e974a1b4e7178329ed37c192c30f8e1868696511
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2020-09-25 20:24:29 -0500

    cleaned up, moved several keypress lines into separate functions, show spacebar when target letter is a space, styled target-letter-space in css

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 // GLOBAL HIDE UPPER KEYBOARD ON PAGE LOAD
 $('#keyboard-upper-container').hide();
 
-let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 
-                    'Too ato too nOt enot one totA not anot tOO aNot', 
-                    'oat itain oat tain nate eate tea anne inant nean', 
-                    'itant eate anot eat nato inate eat anot tain eat', 
+// let sentences = ['ten ate ', 
+//                     'Too ato ', 
+//                     'oat itain ', 
+//                     'itant eate ', 
+//                     'nee ene'];
+
+
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate ', 
+                    'Too ato too nOt enot one totA not anot tOO aNot ', 
+                    'oat itain oat tain nate eate tea anne inant nean ', 
+                    'itant eate anot eat nato inate eat anot tain eat ', 
                     'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+// let sentences = ['tttttttttttttttttttttttttttttttttttttttttttttttt ', 
+//                     'Ttttttttttttttttttttttttttttttttttttttttttttttt ', 
+//                     'otttttttttttttttttttttttttttttttttttttttttttttt ', 
+//                     'itttttttttttttttttttttttttttttttttttttttttttttt ', 
+//                     'nttttttttttttttttttttttttttttttttttttttttttttt'];
 
 let i = 0;
 let j = 0;
 let q = 1;
-letterCounter = 0;
-$('#sentence').append(sentences[i]);
+let letterCounter = 0;
 let nextLetter = sentences[i][j];
-// let nextLetterPlus = sentences[i][q]
+$('#sentence').append(sentences[i]);
 $('#target-letter').append(nextLetter)
 
 
@@ -36,79 +48,96 @@ $(document).keydown(function(e) {
 // HIGHLIGHT KEYS IN BROWSER WHEN PRESSED ON KEYBOARD, WAIT, REMOVE
 $(document).keypress(function(event) {
     let keyPress = event.keyCode || event.which;
-    // console.log(keyPress);
-    $(`#${keyPress}`).css({backgroundColor: 'mediumslateblue'});
-    setTimeout(() => {
-        $(`#${keyPress}`).css({backgroundColor: 'rgb(0,0,0,0'});
-    }, 100);
-    
+    highlightPressedKey(keyPress);
     getNextLetter();
-    getNextExpectedLetter();
-    $('#target-letter').empty()
-    $('#target-letter').append(nextExpectedLetter)
-
-    if (keyPress == nextLetter.charCodeAt(0)) {
-        // console.log('yes');
-        // placeGlyphicon();
-        
-        let $glyphOk = $('<span>').addClass('glyphicon glyphicon-ok')
-        console.log('yes');
-        $('#feedback').append($glyphOk);
-        
-    } else {
-        let $glyphRemove = $('<span>').addClass('glyphicon glyphicon-remove')
-        console.log('no');
-        $('#feedback').append($glyphRemove);
-    };
+    // getNextExpectedLetter();
+    placeNextExpectedLetter();
+    placeGlyphicon(keyPress);
     
-    
+    // Experiencing some lag after moving the if statement an letterCounter++ here.
+    // If it continues, move back to resetDisplay function.
     letterCounter++;
     if (letterCounter == sentences[i].length) {
-        $('#sentence').empty();
-        $('#feedback').empty();
-        i++;
-        j = 0;
-        letterCounter = 0;
-        $('#sentence').append(sentences[i]);
+        resetDisplay();
     };
 });
 
-
+function highlightPressedKey(keyPress) {
+    $(`#${keyPress}`).css({backgroundColor: 'rgb(150, 140, 240'});
+    setTimeout(() => {
+        $(`#${keyPress}`).css(
+            {backgroundColor: 'rgb(150, 140, 240, 0'}
+        );
+    }, 100);
+};
 
 function getNextLetter() {
     nextLetter = sentences[i][j];
-    // console.log(nextLetter);
+    nextExpectedLetter = sentences[i][q];
     j++;
-    return nextLetter;
+    q++;
+    // return nextLetter;
 };
 
 //This function needs to return nextLetter at start of each sentence and 
 //nextExpectedLetter the rest of the time
 //needs to return '<space>' on empty string
 function getNextExpectedLetter() {
-    nextExpectedLetter = sentences[i][q];
-    q++;
-    return nextExpectedLetter;
+    // return nextExpectedLetter;
+};
+
+function placeNextExpectedLetter() {
+    $('#target-letter').empty();
+    
+    if (nextExpectedLetter == ' ') {
+        let space = $('<div id = target-letter-space> spacebar </div>')
+        $('#target-letter').append(space);
+    } else {
+        $('#target-letter').append(nextExpectedLetter)
+    };
 }
 
 
-// let $glyphOk = $('<span>').addClass('glyphicon glyphicon-ok')
-
-function placeGlyphicon() {
-    let $glyphOk = $('<span>').addClass('glyphicon glyphicon-ok')
-    let $glyphRemove = $('<span>').addClass('glyphicon glyphicon-remove')
+function placeGlyphicon(keyPress) {
     if (keyPress == nextLetter.charCodeAt(0)) {
-        console.log('yes');
+        let $glyphOk = $('<span>').addClass('glyphicon glyphicon-ok')
         $('#feedback').append($glyphOk);
     } else {
-        console.log('no');
+        let $glyphRemove = $('<span>').addClass('glyphicon glyphicon-remove')
         $('#feedback').append($glyphRemove);
     };
 };
 
+function resetDisplay() {
+    // letterCounter++;
+    // if (letterCounter == sentences[i].length) {
+    $('#sentence').empty();
+    $('#feedback').empty();
+    i++;
+    j = 0;
+    q = 1;
+    letterCounter = 0;
+    $('#sentence').append(sentences[i]);
 
+    nextLetter = sentences[i][j];
+    $('#target-letter').append(nextLetter);
+    // return i;
+};
 
+// };
 
 // setTimeout(() => { }, 1000);
 
 
+// THIS WAS IN KEYPRESS FUNCTION. REPLACED WITH placeGlyphicon(keyPress)
+// if (keyPress == nextLetter.charCodeAt(0)) {
+    // console.log('yes');
+    // };    
+    //     let $glyphOk = $('<span>').addClass('glyphicon glyphicon-ok')
+    //     $('#feedback').append($glyphOk);
+    
+    // } else {
+        //     let $glyphRemove = $('<span>').addClass('glyphicon glyphicon-remove')
+    //     $('#feedback').append($glyphRemove);
+    // };
+    
