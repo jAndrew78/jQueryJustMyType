69dc1e4ef0410237018104e181e9c1cd55aba4e7
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2020-09-27 12:15:21 -0500

    tidying up the place, comments and organization

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-// GLOBAL HIDE UPPER KEYBOARD ON PAGE LOAD
-$('#keyboard-upper-container').hide();
-
+// GLOBAL ENDGAME VARIABLES
 let startTime;
 let endGame = false;
 let startTiming = false;
@@ -10,27 +8,16 @@ let wordsPerMinute;
 let numberOfMistakes = 0;
 
 
-// SHORT SENTENCES FOR CONVENIENCE IN TESTING
-// let sentences = ['ten ate ', 
-//                     'Too ato ', 
-//                     'oat itain ', 
-//                     'itant eate ', 
-//                     'nee ene',];
-
+// GLOBAL SENTENCES ARRAY
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate ', 
                     'Too ato too nOt enot one totA not anot tOO aNot ', 
                     'oat itain oat tain nate eate tea anne inant nean ', 
                     'itant eate anot eat nato inate eat anot tain eat ', 
                     'nee ene ate ite tent tiet ent ine ene ete ene ate',
-];
+];                    
 
-// TESTING FOR LAG
-// let sentences = ['tttttttttttttttttttttttttttttttttttttttttttttttt ', 
-//                     'Ttttttttttttttttttttttttttttttttttttttttttttttt ', 
-//                     'otttttttttttttttttttttttttttttttttttttttttttttt ', 
-//                     'itttttttttttttttttttttttttttttttttttttttttttttt ', 
-//                     'nttttttttttttttttttttttttttttttttttttttttttttt'];
 
+// GLOBAL COUNTERS, ETC
 let i = 0;
 let j = 0;
 let q = 1;
@@ -39,15 +26,18 @@ let letterCounter = 0;
 let sentenceCounter = 0;
 let yellowBlockCounter = 0;
 let nextLetter = sentences[i][j];
-$('#sentence').append(sentences[i]);
-$('#target-letter').append(nextLetter);
 
+
+// GLOBAL AUDIO
 let typewriterKeystrokeAudio = new Audio('typewriterKeystroke.wav');
 let typewriterBellAudio = new Audio('typewriterBell.wav');
 let typewriterBellAndCarriageAudio = new Audio('typewriterBellAndCarriage.wav');
 
-// setTimeout(() => { }, 1000);
 
+// ON PAGE LOAD: HIDE UPPERCASE KEYBOARD, APPEND FIRST SENTENCE/FIRST TARGET LETTER
+$('#keyboard-upper-container').hide();
+$('#sentence').append(sentences[i]);
+$('#target-letter').append(nextLetter);
 
 
 // TOGGLE UPPER AND LOWER KEYBOARDS ON SHIFT KEYDOWN/UP
@@ -64,48 +54,47 @@ $(document).keydown(function(e) {
 });
 
 
-// EVERYTHING THAT HAPPENS WHEN A KEY IS PRESSED
+// DRIVE THE GAME BASED ON EVERYTHING THAT HAPPENS WHEN A KEY IS PRESSED
 $(document).keypress(function(event) {
     let keyPress = event.keyCode || event.which;
 
-    typewriterKeystrokeAudio.play();
-    
+    // GAME FUNCTIONS TO RUN ON EVERY KEYSTROKE 
     highlightPressedKey(keyPress);
+    typewriterKeystrokeAudio.play();
 
-    // NEED TO RESET THIS WHEN THE GAME RESTARTS
+
+    // START TIMING ON FIRST KEYSTROKE ONLY
     if (startTiming == false) {
         startTiming = true;
         startTime = new Date().getTime();
     };
-
     
-    // NEED TO RESET ENDGAME TO FALSE SOMEWHERE
-    // NEED TO REMOVE TEXT-ALIGN CENTER FROM #SENTENCES ON RESET
-    // NEED TO PUT YELLOW BOX BACK
-    // PROBABLY NEED TO PUT STARTING LETTER BACK IN #TARGET-LETTER
-
-    
+    // GAME FUNCTIONS TO RUN ON EVERY KEYSTROKE UNTIL WE HIT ENDGAME
     if (endGame == false) {
-        // sentences[i][j].css(backgroundColor, 'mediumslateblue')
         getNextLetter();
         placeNextExpectedLetter();
         placeGlyphicon(keyPress);
         moveYellowBlock();
-
-        // Experiencing some lag after moving the if statement and letterCounter++ here.
+        
+        // Experiencing some lag after moving the if statement & letterCounter++ here.
         // If it continues, move back to resetDisplay function.
         letterCounter++;
         if (letterCounter == sentences[i].length) {
             resetDisplay();
         };
     };    
-
-
+    
+        
+        // INITIAL THOUGHTS ON RESETTING THE GAME INSTEAD OF RELOADING: 
+        // NEED TO RESET ENDGAME TO FALSE SOMEWHERE
+        // NEED TO REMOVE TEXT-ALIGN CENTER FROM #SENTENCES ON RESET
+        // NEED TO PUT YELLOW BOX BACK
+        // PROBABLY NEED TO PUT STARTING LETTER BACK IN #TARGET-LETTER
 });
 
 
 
-// HIGHLIGHT KEYS IN BROWSER WHEN PRESSED ON KEYBOARD, WAIT, SET TO TRANSPARENT
+// HIGHLIGHT KEYS IN BROWSER WHEN PRESSED ON KEYBOARD, WAIT, REMOVE HIGHLIGHTING
 function highlightPressedKey(keyPress) {
     $(`#${keyPress}`).css('border-color', 'rgb(0, 0, 0)');
     setTimeout(() => {
